deb96a9d438f9926f9e9816e20c2ca20e924ff4b
tianjidong <tianjidong@jd.com>
2017-06-01 10:32:44 +0800

    更改结构

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mybatis
 mybatis3.3.0 中文注释并附带图解架构
+=======
+Edison-Mybatis
+====
+Mybatis中文注释，以及结构流程图
---
+++ src/main/java/com/jd/edison/demo/Demo.java
@@ -0,0 +1,20 @@
+package com.jd.edison.demo;
+
+import com.jd.edison.mybatis.io.Resources;
+import com.jd.edison.mybatis.session.SqlSessionFactory;
+import com.jd.edison.mybatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+
+/**
+ * @author 田继东 on 2017/5/24.
+ */
+public class Demo {
+    public static void main(String[] args) {;
+        try {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
